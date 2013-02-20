1f07b1366fcba11689629c6721a6ae8c149b80dd
Victor Boctor <vboctor@gmail.com>
2013-02-19 22:16:29 -0800

    Cleanup of Enumeration class.
    1. Follow new coding standard.
    2. Don't check then fetch from dictionary, use TryGetValue().
    3. After splitting enum string by commas, skip blank segments.

--- mantisconnect/Enumeration.cs
+++ mantisconnect/Enumeration.cs
@@ -13,13 +13,13 @@
 // </summary>
 //-----------------------------------------------------------------------
 
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-
 namespace Futureware.MantisConnect
 {
-	/// <summary>
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+
+    /// <summary>
 	/// A class to handle enumerations that are defined in Mantis.
 	/// </summary>
 	/// <remarks>
@@ -49,14 +49,14 @@ namespace Futureware.MantisConnect
         /// <param name="enumeration">Enumeration string to work with.</param>
         public MantisEnum(string enumeration)
         {
-            if (String.IsNullOrEmpty(enumeration))
+            if (string.IsNullOrEmpty(enumeration))
             {
                 throw new ArgumentNullException(enumeration);
             }
 
             this.enumeration = enumeration;
 
-            string[] entries = enumeration.Split(',');
+            string[] entries = enumeration.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
 
             this.labelToCodeMap = new Dictionary<string, int>(entries.Length);
             this.codeToLabelMap = new Dictionary<int, string>(entries.Length);
@@ -66,31 +66,37 @@ namespace Futureware.MantisConnect
 
             foreach (string entry in entries)
             {
-                string[] details = entry.Split(':');
+                string trimmedEntry = entry.Trim();
+                if (trimmedEntry.Length == 0)
+                {
+                    continue;
+                }
+
+                string[] details = trimmedEntry.Split(':');
                 if (details.Length != 2)
                 {
-                    throw new FormatException(String.Format(CultureInfo.InvariantCulture, "Invalid enumeration '{0}'.", enumeration));
+                    throw new FormatException(string.Format(CultureInfo.InvariantCulture, "Invalid enumeration '{0}'.", enumeration));
                 }
 
                 if (!Int32.TryParse(details[0].Trim(), out code))
                 {
-                    throw new FormatException(String.Format(CultureInfo.InvariantCulture, "Invalid code '{0}' in enumeration '{1}'.", details[0], enumeration));
+                    throw new FormatException(string.Format(CultureInfo.InvariantCulture, "Invalid code '{0}' in enumeration '{1}'.", details[0], enumeration));
                 }
 
                 label = details[1].Trim();
                 if (label.Length == 0)
                 {
-                    throw new FormatException(String.Format(CultureInfo.InvariantCulture, "Code '{0}' has an empty label in enumeration '{1}'", code, enumeration));
+                    throw new FormatException(string.Format(CultureInfo.InvariantCulture, "Code '{0}' has an empty label in enumeration '{1}'", code, enumeration));
                 }
 
                 if (this.labelToCodeMap.ContainsKey(label))
                 {
-                    throw new FormatException(String.Format(CultureInfo.InvariantCulture, "Label '{0}' exists more than once in enumeration '{1}'", label, enumeration));
+                    throw new FormatException(string.Format(CultureInfo.InvariantCulture, "Label '{0}' exists more than once in enumeration '{1}'", label, enumeration));
                 }
 
                 if (this.codeToLabelMap.ContainsKey(code))
                 {
-                    throw new FormatException(String.Format(CultureInfo.InvariantCulture, "Code '{0}' exists more than once in enumeration '{1}'", code, enumeration));
+                    throw new FormatException(string.Format(CultureInfo.InvariantCulture, "Code '{0}' exists more than once in enumeration '{1}'", code, enumeration));
                 }
 
                 this.labelToCodeMap[label] = code;
@@ -107,12 +113,14 @@ namespace Futureware.MantisConnect
         {
             get 
             {
-                if (this.codeToLabelMap.ContainsKey(id))
+                string name;
+
+                if (!this.codeToLabelMap.TryGetValue(id, out name))
                 {
-                    return this.codeToLabelMap[id];
+                    name = string.Format(CultureInfo.InvariantCulture, "@{0}@", id);
                 }
 
-                return string.Format(CultureInfo.InvariantCulture, "@{0}@", id);
+                return name;
             }
         }
 
@@ -125,12 +133,14 @@ namespace Futureware.MantisConnect
         {
             get 
             {
-                if (this.labelToCodeMap.ContainsKey(name))
+                int id;
+
+                if (!this.labelToCodeMap.TryGetValue(name, out id))
                 {
-                    return this.labelToCodeMap[name];
+                    id = 0;
                 }
 
-                return 0;
+                return id;
             }
         }
 
@@ -153,7 +163,7 @@ namespace Futureware.MantisConnect
             string[] labels = new string[keys.Count];
 
             int i = 0;
-            foreach ( string label in keys ) 
+            foreach (string label in keys)
             {
                 labels[i++] = label;
             }
